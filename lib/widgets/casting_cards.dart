c377cd06214a4860e2b398b8fcffff17514b2ae5
SrDaviid <luis.david.08@hotmail.com>
2023-05-05 15:40:35 -0400

    add customs widgets

---
+++ lib/widgets/casting_cards.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CastingCards extends StatelessWidget {
+  const CastingCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 180,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, int index) {
+          return _CastCard();
+        },
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: 100,
+      height: 100,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/150x300'),
+              height: 140,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            'actor.name => John Cenaaaaaaaaaaaaaa',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
