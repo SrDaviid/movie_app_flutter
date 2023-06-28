29ebf0b671fead1df4b12e52f78bdcca3d637283
SrDaviid <luis.david.08@hotmail.com>
2023-06-28 10:52:27 -0400

    Funcionality of card swiper and movie slider done

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,30 +1,44 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import '../models/movie_models.dart';
 
 class CardSwiper extends StatelessWidget {
+  final List<Movie> movies;
+
+  const CardSwiper({super.key, required this.movies});
+
   @override
   Widget build(BuildContext context) {
     //Permite que el height que implementes se adapte a cualquier tipo de pantalla
     final size = MediaQuery.of(context).size;
 
-    return Container(
+    if (movies.isEmpty) {
+      return SizedBox(
+        width: double.infinity,
+        height: size.height * 0.5,
+        child: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    return SizedBox(
       width: double.infinity,
       //Esta propiedad permite obtener la mitad de la pantalla
       height: size.height * 0.5,
       child: Swiper(
-        itemCount: 10,
+        itemCount: movies.length,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
         itemBuilder: (_, int index) {
+          final movie = movies[index];
           return GestureDetector(
-            onTap: () => Navigator.pushNamed(context, 'details',
-                arguments: 'movie-instance'),
+            onTap: () =>
+                Navigator.pushNamed(context, 'details', arguments: movie),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImage),
                 fit: BoxFit.cover,
               ),
             ),
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,30 +1,65 @@
 import 'package:flutter/material.dart';
 
-class MovieSlider extends StatelessWidget {
-  const MovieSlider({super.key});
+import '../models/movie_models.dart';
+
+class MovieSlider extends StatefulWidget {
+  final List<Movie> movies;
+  final String? title;
+  final Function onNextPage;
+
+  const MovieSlider(
+      {super.key, required this.movies, this.title, required this.onNextPage});
+
+  @override
+  State<MovieSlider> createState() => _MovieSliderState();
+}
+
+class _MovieSliderState extends State<MovieSlider> {
+  final ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent) {
+        widget.onNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
+      child: SizedBox(
         width: double.infinity,
         height: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+            if (widget.title != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Text(
-                  'Popular',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )),
-                const SizedBox(height: 5,),
+                  widget.title!,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            const SizedBox(
+              height: 5,
+            ),
             Expanded(
               child: ListView.builder(
+                controller: scrollController,
                 scrollDirection: Axis.horizontal,
-                itemCount: 120,
+                itemCount: widget.movies.length,
                 itemBuilder: (_, int index) {
-                  return _MoviePoster();
+                  return _MoviePoster(widget.movies[index]);
                 },
               ),
             ),
@@ -36,6 +71,9 @@ class MovieSlider extends StatelessWidget {
 }
 
 class _MoviePoster extends StatelessWidget {
+  final Movie movie;
+
+  const _MoviePoster(this.movie);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,13 +82,13 @@ class _MoviePoster extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(children: [
         GestureDetector(
-          onTap: () => Navigator.pushNamed(context, 'details',
-              arguments: 'movie-instance'),
+          onTap: () =>
+              Navigator.pushNamed(context, 'details', arguments: movie),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: const FadeInImage(
-              placeholder: AssetImage('assets/no-image.jpg'),
-              image: NetworkImage('https://via.placeholder.com/300x400'),
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/no-image.jpg'),
+              image: NetworkImage(movie.fullPosterImage),
               width: 130,
               height: 190,
               fit: BoxFit.cover,
@@ -60,8 +98,8 @@ class _MoviePoster extends StatelessWidget {
         const SizedBox(
           height: 5,
         ),
-        const Text(
-          'Spiderman: The Ultimate Battle Power',
+        Text(
+          movie.originalTitle,
           //Hace que cuando se corte el texto col
           overflow: TextOverflow.ellipsis,
           maxLines: 2,
